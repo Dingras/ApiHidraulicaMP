f37218794b7f352e054f94edc7d9ddd1292de198
dingras <feracosentino@gmail.com>
2024-09-04 22:34:51 -0300

    Implementacion administrador actualizacion

--- MyStock/settings.py
+++ MyStock/settings.py
@@ -158,6 +158,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+LOGIN_URL = '/'
+
 # REST_FRAMEWORK = {
 #     'DEFAULT_SCHEMA_CLASS' : 'rest_framework.schemas.coreapi.AutoSchema',
 # }
--- Products/views.py
+++ Products/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from Products.models import Category, Product
 
+@login_required(login_url='/')
 def product_detail(request, id):
     data_product = Product.objects.get(pk = id)
     return render(request, 'product_detail.html', {'product' : data_product})
 
+@login_required(login_url='/')
 def products_view(request, id):
     data_products = Product.objects.filter(category_id = id)
     data_category = Category.objects.get(pk = id)
     return render(request, 'products.html', {'products' : data_products, 'category':data_category})
 
+@login_required(login_url='/')
 def Categories_View(request):
     data_categories = Category.objects.all()
     return render(request, 'categories.html',{'categories' : data_categories})
