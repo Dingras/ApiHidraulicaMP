5744c0980bf86ce478aba10b16699af845e2511e
dingras <feracosentino@gmail.com>
2024-10-05 08:44:45 -0300

    Gran actualizacion, funcion de shop y ventas

--- Sales/urls.py
+++ Sales/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.sales_view, name='sales'),
+    path('delete/<int:id>/',views.sale_delete_view, name='sale_delete'),
+
 ]
--- Sales/views.py
+++ Sales/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from Sales.models import Sale
 
@@ -7,3 +7,9 @@ from Sales.models import Sale
 def sales_view(request):
     data_sales = Sale.objects.all()
     return render(request, 'sales.html', {'sales': data_sales})
+
+@login_required(login_url='/')
+def sale_delete_view(request, id):
+    sale = get_object_or_404(Sale, pk=id)
+    sale.delete()
+    return sales_view(request)
