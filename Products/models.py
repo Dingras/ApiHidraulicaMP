b7871e86a79ea6826a8cca3be4942238b5230326
dingras <feracosentino@gmail.com>
2025-01-25 19:32:27 -0300

    Cambio max_length --> url_img [Product]

--- Products/models.py
+++ Products/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete= models.CASCADE)
     price = models.DecimalField(max_digits=12, decimal_places=2, null=False)
     count = models.IntegerField(default=0)
-    url_img = models.CharField(max_length=100, null=True)
+    url_img = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
